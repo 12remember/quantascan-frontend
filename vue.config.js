0ce29d0c16a356073812e68fab94e42888c918a3
12remember <nieuw_email@example.com>
2025-01-27 21:14:44 +0100

    Signed-off-by: 12remember <nieuw_email@example.com>

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,7 @@ module.exports = {
       baseURL: process.env.BASE_URL || 'https://www.quantascan.io',
     },
   },
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   filenameHashing: true, // Enable hashed filenames
   productionSourceMap: false, // Optional: Disable source maps for smaller builds
 };
